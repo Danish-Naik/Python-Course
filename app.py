156c2165a0d5c0c2a973d431980661f6c2bf5b2e
Danish-Naik <naikdanish72@gmail.com>
2025-12-12 04:10:33 +0530

    Initial Stage of Learning Python. Learnt about the Python Basic, Implemenatations, etc.

---
+++ app.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+print("I started with the Python Course!! (Finally)")
+print("Still don't know why it has to be every time --Hello World!--")
+
+print('Python2 cannot be run on Python3')
+
+# PyLint is used for identifying the errors in the code.
+print("PEP is a rulebook is used for giving guidelines for Python")
+
+print("There arr different Implemenation for the Python such as CPython, PyPy, Jython,etc just as different OS such as Windows, Linux, etc")
+
+print("The fun part of the Implementation is the Python that is written in Jython will be converted into the Java Bytcode and that Java Bytecode can be understood by the JVM.")
+
+print("Learnt a lot in this stage about the Basics of Python, Implementation of Python and the VS Code Setup")
