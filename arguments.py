37adee5332c160157b181a4c4143ef6a3bf544f6
Danish-Naik <naikdanish72@gmail.com>
2026-01-04 23:31:22 +0530

    More addition to the types of the arguments.

--- arguments.py
+++ arguments.py
@@ -2,5 +2,20 @@
 def add(a, b):
     return a + b
 
-
 print(add(15,18))#Passing arguments by position
+
+#Specified Arguments
+#Used for more than teo arguments to define the value while passed.print
+
+def increment(number, by):
+    return number +by 
+
+print(increment(2, by=1)) #Passing arguments by specified name
+
+#Default Arguments 
+#When the value for the argumnets is fixed such that we can set a default value for the argument
+
+def increment(number, by=1):
+    return number + by
+
+print(increment(2,5)) 
